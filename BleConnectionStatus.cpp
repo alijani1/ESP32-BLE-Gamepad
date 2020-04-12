2c2e98ab28cae1a80cb092a01203758b5d541f9d
Ali Jani <ajanimail@gmail.com>
2020-04-11 23:01:28 -0400

    fixed reconnect status for joystick2

--- BleConnectionStatus.cpp
+++ BleConnectionStatus.cpp
@@ -9,10 +9,9 @@ void BleConnectionStatus::onConnect(BLEServer* pServer)
 
   this->connected = true;
   BLE2902* desc = (BLE2902*)this->inputGamepad->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
-      uint8_t val[] = {0x01, 0x00};
-      desc->setValue(val, 2);
-
   desc->setNotifications(true);
+  BLE2902* desc2 = (BLE2902*)this->inputGamepad2->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
+  desc2->setNotifications(true);
 }
 
 void BleConnectionStatus::onDisconnect(BLEServer* pServer)
@@ -22,4 +21,6 @@ void BleConnectionStatus::onDisconnect(BLEServer* pServer)
   this->connected = false;
   BLE2902* desc = (BLE2902*)this->inputGamepad->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
   desc->setNotifications(false);
+  BLE2902* desc2 = (BLE2902*)this->inputGamepad2->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
+  desc2->setNotifications(false);
 }
