abc5c10825bed27a073192fa8204063ddbe34995
Ali Jani <ajanimail@gmail.com>
2020-04-12 00:14:48 -0400

    fix attempt 2

--- BleConnectionStatus.cpp
+++ BleConnectionStatus.cpp
@@ -8,7 +8,6 @@ void BleConnectionStatus::onConnect(BLEServer* pServer)
   this->connected = true;
   BLE2902* desc = (BLE2902*)this->inputGamepad->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
   desc->setNotifications(true);
-  delay(10);
   desc = (BLE2902*)this->inputGamepad2->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
   desc->setNotifications(true);
 }
@@ -18,7 +17,6 @@ void BleConnectionStatus::onDisconnect(BLEServer* pServer)
   this->connected = false;
   BLE2902* desc = (BLE2902*)this->inputGamepad->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
   desc->setNotifications(false);
-  delay(10);
   desc = (BLE2902*)this->inputGamepad2->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
   desc->setNotifications(false);
 }
