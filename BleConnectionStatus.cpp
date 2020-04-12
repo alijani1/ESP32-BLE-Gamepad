d0e8a0761fcc630117c2bd04f65ccf04acc665c0
Ali Jani <ajanimail@gmail.com>
2020-04-11 22:45:29 -0400

    reconncection fix attempt

--- BleConnectionStatus.cpp
+++ BleConnectionStatus.cpp
@@ -5,13 +5,20 @@ BleConnectionStatus::BleConnectionStatus(void) {
 
 void BleConnectionStatus::onConnect(BLEServer* pServer)
 {
+  Serial.println("connected");
+
   this->connected = true;
   BLE2902* desc = (BLE2902*)this->inputGamepad->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
+      uint8_t val[] = {0x01, 0x00};
+      desc->setValue(val, 2);
+
   desc->setNotifications(true);
 }
 
 void BleConnectionStatus::onDisconnect(BLEServer* pServer)
 {
+  Serial.println("disconnected");
+
   this->connected = false;
   BLE2902* desc = (BLE2902*)this->inputGamepad->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
   desc->setNotifications(false);
