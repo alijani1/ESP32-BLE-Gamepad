5665d6565c8660e56a0c65af6f986272242e61cc
Ali Jani <ajanimail@gmail.com>
2020-04-12 00:21:13 -0400

    commented out change

--- BleConnectionStatus.cpp
+++ BleConnectionStatus.cpp
@@ -8,8 +8,8 @@ void BleConnectionStatus::onConnect(BLEServer* pServer)
   this->connected = true;
   BLE2902* desc = (BLE2902*)this->inputGamepad->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
   desc->setNotifications(true);
-  desc = (BLE2902*)this->inputGamepad2->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
-  desc->setNotifications(true);
+  // desc = (BLE2902*)this->inputGamepad2->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
+  // desc->setNotifications(true);
 }
 
 void BleConnectionStatus::onDisconnect(BLEServer* pServer)
@@ -17,6 +17,6 @@ void BleConnectionStatus::onDisconnect(BLEServer* pServer)
   this->connected = false;
   BLE2902* desc = (BLE2902*)this->inputGamepad->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
   desc->setNotifications(false);
-  desc = (BLE2902*)this->inputGamepad2->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
-  desc->setNotifications(false);
+  // desc = (BLE2902*)this->inputGamepad2->getDescriptorByUUID(BLEUUID((uint16_t)0x2902));
+  // desc->setNotifications(false);
 }
